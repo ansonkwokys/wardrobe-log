657effe06efe1039548b883eeeab6654c17183e6
ansonkwokys <ansonyskwok@gmail.com>
2024-12-28 10:35:02 -0500

    fix(auth): update navbar rendering logic, add Login.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar.jsx";
 import Wardrobe from "./components/Wardrobe.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 function App() {
+    let location = useLocation();
     return (
         <BrowserRouter className="app-container">
-            <NavBar className="nav-bar"/>
+            {location.pathname !== "/" && <NavBar className="nav-bar"/>}
             <Routes>
+                <Route path="/" element={<Login />} />
                 <Route path="/wardrobe" element={<Wardrobe />} />
             </Routes>
         </BrowserRouter>
