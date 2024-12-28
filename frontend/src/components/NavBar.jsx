493340e7fd43aeb00a43257e7e461d8701a729d2
ansonkwokys <ansonyskwok@gmail.com>
2024-12-28 01:06:44 -0500

    feat(frontend-wardrobe): add interface for wardrobe page
    - Set up React with Vite.
    - Designed the wardrobe page layout in HTML.
    - Styled the page using CSS.

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+            <div className="nav-container">
+                <div className="nav-logo">
+                    <a href="/wardrobe">WARDROBE-LOG</a>
+                </div>
+                <ul className="nav-links">
+                    <li>
+                        <a href="/wardrobe">WARDROBE</a>
+                    </li>
+                    <li>
+                        <a href="#">OUTFIT</a>
+                    </li>
+                    <li>
+                        <a href="#">THREAD</a>
+                    </li>
+                </ul>
+                <ul className="nav-auth">
+                    <li>
+                        <a href="#">Settings</a>
+                    </li>
+                    <li>
+                        <form action="/logout" method="POST">
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
