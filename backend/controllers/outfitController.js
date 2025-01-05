90c80c367431820bd706c4dc6249f8faa564151a
ansonkwokys <ansonyskwok@gmail.com>
2025-01-05 16:22:31 -0500

    feat(backend-outfit):s3 bucket set up for outfit

--- backend/controllers/outfitController.js
+++ backend/controllers/outfitController.js
@@ -1,4 +1,18 @@
 require("dotenv").config();
 const {
-    
-}
+    PutObjectCommand,
+    GetObjectCommand,
+    S3Client,
+    DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
+const outfitService = require("../services/outfitService.js");
+const outfitQueries = require("../db/queries/outfitQueries.js");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const outfitBucketName = process.env.BUCKET_NAME;
+const outfitBucketRegion = process.env.BUCKET_REGION;
+const outfitBucketAccessKey = process.env.BUCKET_ACCESS_KEY;
+const outfitBucketSecretAccessKey = proecess.env.BUCKET_SECRET_ACCESS_KEY;
+    process.env.BUCKET_SECRET_ACCESS_KEY;
+
+const prefix = "service/";
