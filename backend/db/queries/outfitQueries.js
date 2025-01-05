b57696d280dcd85a7cc5f5fa6b1f764291857922
ansonkwokys <ansonyskwok@gmail.com>
2025-01-05 18:44:09 -0500

    feat(backend-outfit): add outfit queries necessary for inserting new outfit

---
+++ backend/db/queries/outfitQueries.js
@@ -0,0 +1,21 @@
+const pool = require("../pool");
+const OUTFIT_TABLE = "outfit";
+const OUTFIT_CLOTHING_ITEM_TABLE = "outfit_clothing_item";
+
+exports.insertNewOutfit = async ({ userId, s3ImageKey }) => {
+    await pool.query(
+        `INSERT INTO ${OUTFIT_TABLE} (user_id, s3_image_key) VALUES ($1, $2)`,
+        [userId, s3ImageKey]
+    );
+};
+
+exports.insertWardrobeClothingItemToOutfit = async ({
+    outfitId,
+    clothingId,
+    categoryId,
+}) => {
+    await pool.query(
+        `INSERT INTO ${OUTFIT_CLOTHING_ITEM_TABLE} (outfit_id, clothing_id, category_id) VALUES ($1, $2, $3)`,
+        [outfitId, clothingId, categoryId]
+    );
+};
